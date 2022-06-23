ff65afac3c2921b05d9a1a5f2ea9dd2e7227717f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-06-23 11:56:56 +0530

    comit

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,6 @@ import "./App.css"
 
 export default function Home(){
     return(
-        <h1 style={{ backgroundColor:'skyblue',margin:'0px' ,display:"flex",justifyContent:'center',alignItems:'center',height:'100vh',fontSize:"64px", color:'white'}}>Welcome to &nbsp;<span style={{color:'blue'}} className="glow" ><a href="https://cycatz.com/" target="_blank" style={{ textDecoration:'none'}}>Cycatz</a></span></h1>
+        <div style={{display:'flex'}} ><h1 className="Home" >Welcome &nbsp;<span>to</span> &nbsp;<span style={{color:'blue'}} className="glow" ><a href="https://cycatz.com/" target="_blank" style={{ textDecoration:'none'}}>Cycatz</a></span></h1></div>
     );
 }
